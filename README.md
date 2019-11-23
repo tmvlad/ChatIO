8ec12022238fce9f2db670e87d639849cfa41098
VladTheia <vlad.theia.madalin@gmail.com>
2019-11-23 18:50:56 +0200

    adds chatIO project

---
+++ README.md
@@ -0,0 +1,15 @@
+#ChatIO server implementation
+
+Simple real-time chat app, using the JavaScript Socket.IO library.
+
+## Usage
+
+```bash
+npm install
+node server.js
+```
+
+Then, open 2 or more tabs on localhost:3000 and start using the app.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log("Server is running");
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', (socket) => {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+    // Disconnect
+    socket.on('disconnect', (data) => {
+        // if (!socket.username) return;
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    }); 
+
+    // Send message
+    socket.on('send message', (data) => {
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+    });
+
+    // New user
+    socket.on('new user', (data, callback) => {
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    })
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+    }
+});
